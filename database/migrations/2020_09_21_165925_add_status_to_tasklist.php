7867bc94f04e22c2de72fb7db9e889492a9bb035
yusuke mitsui <funnyfool14@gmail.com>
2020-09-21 22:13:32 +0000

    再提出2nd

--- database/migrations/2020_09_21_165925_add_status_to_tasklist.php
+++ database/migrations/2020_09_21_165925_add_status_to_tasklist.php
@@ -14,7 +14,7 @@ class AddStatusToTasklist extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-         $table->string('name', 10);
+         $table->string('status', 10);
         });
     }
 
@@ -26,7 +26,7 @@ class AddStatusToTasklist extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('name');
+            $table->dropColumn('status');
         });
     }
 }
